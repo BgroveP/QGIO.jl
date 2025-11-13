2722295d098ed3a514a67f49bbe16e58496f91f6
bgrovep <bgrovepalt@gmail.com>
2025-11-13 12:49:04 +0100

    Fixed bug where the subset function would print loci in wrong order

--- src/subset.jl
+++ src/subset.jl
@@ -13,32 +13,44 @@ function subset(inpath, outpath; samples::Vector{String}=[""], loci::DataFrame=D
     # 
     infile = QGIO.open_vcf(inpath)
     outfile = GZip.open(outpath, "w")
+
     # Print previous header
     for l in QGIO.header(inpath)
         print(outfile, l)
     end
+
     # Print new note
-    print(outfile, "##QGIO.jl=subsetted")
+    println(outfile, "##QGIO.jl=subsetted")
+
     # Subset
     readlock = ReentrantLock()
     writelock = ReentrantLock()
+    writeturn = 1
     @threads for i in 1:nthreads()
         buffer = QGIO.create_buffer()
         notendofffile = false
         @lock readlock notendofffile = QGIO.readline!(infile, buffer)
         while notendofffile
+            # Wait for turn
+            while writeturn != i
+                nothing
+            end
+
+            # If your turn
             if ~all(buffer[1:2] .== UInt8('#'))
-                if (buffer[1] == UInt8('#')) | (get!(locusdict, (QGIO._buffer_chromosome(buffer),QGIO._buffer_identifier(buffer)), false))
-                    t = _buffer_subset(buffer, thissamples)
-                    @lock writelock begin 
+                if (buffer[1] == UInt8('#')) | (get!(locusdict, (QGIO._buffer_chromosome(buffer), QGIO._buffer_identifier(buffer)), false))
+                    t = QGIO._buffer_subset(buffer, thissamples)
+                    @lock writelock begin
                         @views print.(outfile, Char.(buffer[t]))
                         print(outfile, '\n')
                     end
                 end
             end
-        @lock readlock notendofffile = QGIO.readline!(infile, buffer)
+
+            notendofffile = QGIO.readline!(infile, buffer)
+            global writeturn = writeturn == nthreads() ? 1 : writeturn + 1
         end
+
     end
     close(outfile)
 end
-
